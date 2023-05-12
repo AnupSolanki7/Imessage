f33c6ef34e5b3a17ebed1bbd06b8f2fc6ebc7000
AnupWiseSkulls7 <anup@wiseskulls.com>
2023-05-12 18:04:04 +0530

    anup

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -58,12 +58,64 @@ const AuthForm = () => {
           <Input label="Email" register={register} id="email" errors={errors} />
           <Input
             label="Password"
+            type="password"
             register={register}
             id="password"
             errors={errors}
           />
-          <Button/>
+          <Button disabled={loading} fullWidth type="submit">{variant === "LOGIN" ? "Sign In" :  "Register"}</Button>
         </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div 
+              className="
+                absolute 
+                inset-0 
+                flex 
+                items-center
+              "
+            >
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            {/* <AuthSocialButton 
+              icon={BsGithub} 
+              onClick={() => socialAction('github')} 
+            />
+            <AuthSocialButton 
+              icon={BsGoogle} 
+              onClick={() => socialAction('google')} 
+            /> */}
+          </div>
+        </div>
+        <div 
+          className="
+            flex 
+            gap-2 
+            justify-center 
+            text-sm 
+            mt-6 
+            px-2 
+            text-gray-500
+          "
+        >
+          <div>
+            {variant === 'LOGIN' ? 'New to Messenger?' : 'Already have an account?'} 
+          </div>
+          <div 
+            onClick={toggleVariant} 
+            className="underline cursor-pointer"
+          >
+            {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+          </div>
+        </div>
       </div>
     </div>
   );
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,11 +1,43 @@
 import AuthForm from "./components/AuthForm";
+import Image from "next/image";
+
 
 export default function Home() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-slate-200">
-        <img className="mx-auto" width="48px" height="48px" src="/images/logo.png" alt="logo" />
-        <h2 className="text-2xl font-bold" >Sign in to your account</h2>
-        <AuthForm/>
-      </main>
-    )
-  }
+  return (
+    <main
+      className="
+      flex 
+      min-h-full 
+      flex-col 
+      justify-center 
+      py-12 
+      sm:px-6 
+      lg:px-8 
+      bg-gray-100
+    "
+    >
+       <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image
+          height="48"
+          width="48"
+          className="mx-auto w-auto"
+          src="/images/logo.png"
+          alt="Logo"
+        />
+        <h2 
+          className="
+            mt-6 
+            text-center 
+            text-3xl 
+            font-bold 
+            tracking-tight 
+            text-gray-900
+          "
+          >
+            Sign in to your account
+        </h2>
+      </div>
+      <AuthForm />
+    </main>
+  );
+}

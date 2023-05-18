4531eb0488229d7328212b307175c9f8d0c53e8d
AnupWiseSkulls7 <anup@wiseskulls.com>
2023-05-18 10:43:28 +0530

    anup

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -53,10 +53,10 @@ export const authOptions: AuthOptions = {
     })
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  debug: process.env.NODE_ENV === 'development',
-  session: {
-    strategy: "jwt",
-  },
+  // debug: process.env.NODE_ENV === 'development',
+  // session: {
+  //   strategy: "jwt",
+  // },
 }
 
 const handler = NextAuth(authOptions);
